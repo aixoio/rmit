1b4a5df7b5423c24d2f55deadf406ca6b826a6d9
David L Terpin <79969530+aixoio@users.noreply.github.com>
2025-07-17 17:39:31 +1000

    feat: add function to retrieve git diff

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,18 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/go-git/go-git/v6"
 	"github.com/spf13/cobra"
 )
+
+func getGitDiff() (string, error) {
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		return "", fmt.Errorf("current directory is not a git repository: %w", err)
+	}
+
+	// write the rest of the cde
+}
 
 var RootCmd = &cobra.Command{
 	Use:   "rmit",
